a250efcf153dd37a1d38918c2b9f35f507740ceb
jiebin.hu <jiebin.hjb@nonolive.com>
2020-01-21 11:26:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	s := "11弟弟"
-	t := []rune(s)
-	var str string
-	for i, v := range t {
-		str = str + string(v)
-		fmt.Println(i, str)
-	}
+	t := time.Unix(int64(1579004840), 0)
+	t.In(time.UTC)
+	fmt.Println(t.In(time.UTC).Location(), t.In(time.UTC).Hour())
 }
